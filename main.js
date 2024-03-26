d79ae828a53177fd7f69c72c2baa5de13fabd747
Marcos <112193426+Eggsbennedit@users.noreply.github.com>
2024-03-26 07:17:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ for (let i = 0; i < botoes.length; i++) {
 
 // cria um tempo limite para a conclusão dos objetivos
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2024-11-02T00:00:00");
+const tempoObjetivo2 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-31T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-31T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
